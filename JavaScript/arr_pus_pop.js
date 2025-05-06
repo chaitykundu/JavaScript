9209b7f6634a7b06a0d4812bf436d92c83e33d03
Chaity Kundu <97236983+chaitykundu@users.noreply.github.com>
2025-05-06 12:28:50 +0600

    Add files via upload

---
+++ JavaScript/arr_pus_pop.js
@@ -0,0 +1,11 @@
+let subjects = ["c++", "Java", "Python","Javascript"];
+console.log(subjects);
+let addSub = subjects.push("PHP");
+console.log(addSub);
+console.log(subjects);
+
+//pop elements
+let dltSub = subjects.pop();
+console.log(subjects);
+console.log(subjects.toString())
+console.log(subjects);
